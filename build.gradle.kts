6bffab1d6d998401472eb6111a50182ab07eb9dc
vince <me@liuwj.me>
2023-08-06 17:23:17 +0800

    add ksp plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("com.google.devtools.ksp") version "1.7.22-1.0.8"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -11,6 +12,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
+    mavenLocal()
     mavenCentral()
     gradlePluginPortal()
 }
@@ -21,10 +23,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.ktorm:ktorm-core:3.6.0")
-    implementation("org.ktorm:ktorm-jackson:3.6.0")
-    implementation("org.ktorm:ktorm-support-mysql:3.6.0")
     implementation("mysql:mysql-connector-java:8.0.13")
+    implementation("org.ktorm:ktorm-core:3.7.0-SNAPSHOT")
+    implementation("org.ktorm:ktorm-jackson:3.7.0-SNAPSHOT")
+    implementation("org.ktorm:ktorm-support-mysql:3.7.0-SNAPSHOT")
+    implementation("org.ktorm:ktorm-ksp-annotations:3.7.0-SNAPSHOT")
+    ksp("org.ktorm:ktorm-ksp-compiler:3.7.0-SNAPSHOT")
 }
 
 tasks {
@@ -34,4 +38,8 @@ tasks {
             jvmTarget = "1.8"
         }
     }
+}
+
+sourceSets.main {
+    kotlin.srcDir("build/generated/ksp/main/kotlin")
 }
