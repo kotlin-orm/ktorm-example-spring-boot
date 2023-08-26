6b9715a6f7c8780740b58e0c4fa905d9d0583663
vince <me@liuwj.me>
2023-08-26 18:08:54 +0800

    upgrade kotlin to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 plugins {
     id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    id("com.google.devtools.ksp") version "1.7.22-1.0.8"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
 }
 
 group = "org.ktorm"
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
@@ -38,8 +38,4 @@ tasks {
             jvmTarget = "1.8"
         }
     }
-}
-
-sourceSets.main {
-    kotlin.srcDir("build/generated/ksp/main/kotlin")
 }
