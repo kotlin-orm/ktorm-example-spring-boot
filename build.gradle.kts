e40b303502b66c507dd1ca9f3640e07a04580155
vince <me@liuwj.me>
2024-10-26 20:12:21 +0800

    upgrade ktorm version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("mysql:mysql-connector-java:8.0.13")
-    implementation("org.ktorm:ktorm-core:4.1.0")
-    implementation("org.ktorm:ktorm-jackson:4.1.0")
-    implementation("org.ktorm:ktorm-support-mysql:4.1.0")
-    implementation("org.ktorm:ktorm-ksp-annotations:4.1.0")
-    ksp("org.ktorm:ktorm-ksp-compiler:4.1.0")
+    implementation("org.ktorm:ktorm-core:4.1.1")
+    implementation("org.ktorm:ktorm-jackson:4.1.1")
+    implementation("org.ktorm:ktorm-support-mysql:4.1.1")
+    implementation("org.ktorm:ktorm-ksp-annotations:4.1.1")
+    ksp("org.ktorm:ktorm-ksp-compiler:4.1.1")
 }
 
 tasks {
